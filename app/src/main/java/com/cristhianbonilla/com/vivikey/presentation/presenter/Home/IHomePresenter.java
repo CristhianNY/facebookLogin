cc83923335e7b71e5fa19d370de164e63ae8c49f
Cristhian Ariel Bonilla Espinosa - Ceiba Software <cristhian.bonilla@ceiba.com.co>
2019-09-30 07:40:41 -0500

    avances

--- app/src/main/java/com/cristhianbonilla/com/vivikey/presentation/presenter/Home/IHomePresenter.java
+++ app/src/main/java/com/cristhianbonilla/com/vivikey/presentation/presenter/Home/IHomePresenter.java
@@ -4,10 +4,6 @@ import com.cristhianbonilla.com.vivikey.core.domain.User;
 
 import java.util.List;
 
-public class HomePresenter implements IHomePresenter {
-
-    @Override
-    public List<User> getFriends(User user) {
-        return null;
-    }
+public interface IHomePresenter {
+       List<User>getFriends(User user);
 }
